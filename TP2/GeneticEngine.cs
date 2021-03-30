721d175567180c850720deb2e1bb0e0838a20664
Joaquín Colonnello <joaquincolonnello@hotmail.com>
2021-03-30 20:50:40 -0300

    Implemented main loop
    Added database loader

--- TP2/GeneticEngine.cs
+++ TP2/GeneticEngine.cs
@@ -1,12 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
+using TP2.Crossovers;
+using TP2.Finishers;
+using TP2.Models;
+using TP2.Mutations;
+using TP2.Replacements;
+using TP2.Selections;
+using System.Linq;
+using System.Diagnostics;
 
 namespace TP2
 {
     class GeneticEngine
     {
+        public GeneticEngine(ICrossover crossover, IMutation mutation, ISelection parentSelection, IReplacement replacement, IFinisher finish, int k, double pc)
+        {
+            Crossover = crossover;
+            Mutation = mutation;
+            ParentSelection = parentSelection;
+            Replacement = replacement;
+            Finish = finish;
+            K = k;
+            Pc = pc;
+        }
+
+        public ICrossover Crossover { get; }
+        public IMutation Mutation { get; }
+        public ISelection ParentSelection { get; }
+        public IReplacement Replacement { get; }
+        public IFinisher Finish { get; }
+        public int K { get; set; }
+        public double Pc { get; set; }
+
+        IEnumerable<(Character c1, Character c2)> ParentPairings(IEnumerable<Character> parents)
+        {
+            var iter = parents.GetEnumerator();
+            iter.MoveNext();
+            do
+            {
+                Character c1 = iter.Current;
+                iter.MoveNext();
+                Character c2 = iter.Current;
+                yield return (c1, c2);
+            }
+            while (iter.MoveNext());
+        }
+
+        IEnumerable<Character> AdvanceGeneration(IEnumerable<Character> population)
+        {
+            var parents = ParentSelection.Select(population, K);
+            //var scorePo = population.Average(c => c.Fitness);
+            //var scorePa = parents.Average(c => c.Fitness);
+            var children = ParentPairings(parents).SelectMany(pair => Crossover.Crossover(pair.c1, pair.c2));
+            var mutatedChildren = children.Select(c => Mutation.Mutate(c, Pc));
+            var next = Replacement.GetReplacement(population, mutatedChildren);
+            //var scoreRep = next.Average(c => c.Fitness);
+            return next.ToList();
+        }
+
+        IEnumerable<Character> UntilFinish(IEnumerable<Character> population)
+        {
+            int gen = 0;
+            Stopwatch sw = new Stopwatch();
+            var currentPopulation = population;
+            sw.Start();
+            while (!Finish.IsFinished(currentPopulation, gen, sw.ElapsedMilliseconds))
+            {
+                currentPopulation = AdvanceGeneration(currentPopulation);
+                gen++;
+            }
+
+            return currentPopulation;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 1000;
+            int k = 200;
+            int gens = 1000;
+            double pc = 0.05;
+
+            var weapons = ItemTSVLoader.Load("Dataset/allitems/armas.tsv", Item.Type.WEAPON);
+            var boots = ItemTSVLoader.Load("Dataset/allitems/botas.tsv", Item.Type.BOOTS);
+            var helmets = ItemTSVLoader.Load("Dataset/allitems/cascos.tsv", Item.Type.HELMET);
+            var gloves = ItemTSVLoader.Load("Dataset/allitems/guantes.tsv", Item.Type.GLOVES);
+            var chests = ItemTSVLoader.Load("Dataset/allitems/pecheras.tsv", Item.Type.CHEST);
+            Item.LoadItems(weapons.Concat(boots).Concat(helmets).Concat(gloves).Concat(chests));
+
+            var crossover = new TwoPointCrossover();
+            var mutation = new GeneMutation();
+            var parentSelection = new EliteSelection();
+            var populationSelection = new EliteSelection();
+            var replacement = new FillAll(n, populationSelection);
+            var finish = new GenerationFinisher(gens);
+
+            var engine = new GeneticEngine(crossover, mutation, parentSelection, replacement, finish, k, pc);
+            var population = Enumerable.Range(0, n).Select(_ => new Character());
+            var initialScore = population.Sum(c => c.Fitness);
+            Console.WriteLine($"Initial score: {initialScore}");
+            var evolved = engine.UntilFinish(population);
+            var finalScore = evolved.Sum(c => c.Fitness);
+            Console.WriteLine($"Final score: {finalScore}");
+
         }
     }
 }
---
+++ TP2/ItemTSVLoader.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TP2.Models;
+
+namespace TP2
+{
+    public class ItemTSVLoader
+    {
+        public static IEnumerable<Item> Load(string path, Item.Type type)
+        {
+            var sb = new StringBuilder();
+            var lineParser = new ItemLineParser(type);
+            int bufferSize = 20480;
+            List<Item> list = new List<Item>();
+
+            using var reader = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None, bufferSize);
+            while (reader.ReadByte() != '\n') ;
+            while (reader.CanRead)
+            {
+                sb.Clear();
+                while (true)
+                {
+                    var readByte = reader.ReadByte();
+                    switch (readByte)
+                    {
+                        case -1:
+                            goto endOfFile;
+                        case '\r':
+                            continue;
+                        case '\n':
+                            goto endOfLine;
+                        default:
+                            sb.Append((char)readByte);
+                            break;
+                    }
+                }
+            endOfLine:
+
+                list.Add(lineParser.ParseLine(sb));
+            }
+        endOfFile:;
+            return list;
+        }
+
+        //public static IEnumerable<Item> Load(Stream stream, Item.Type type)
+        //{
+        //    using var reader = new TextFieldParser(stream);
+        //    reader.TextFieldType = FieldType.Delimited;
+        //    reader.Delimiters = new string[] { "\t" };
+        //    reader.ReadLine();
+        //    while (!reader.EndOfData)
+        //    {
+        //        var row = reader.ReadFields();
+        //        yield return new Item(int.Parse(row[0]),
+        //                              double.Parse(row[1]),
+        //                              double.Parse(row[2]),
+        //                              double.Parse(row[3]),
+        //                              double.Parse(row[4]),
+        //                              double.Parse(row[5]),
+        //                              type);
+        //    }
+        //}
+
+
+
+        private interface ILineParser<T>
+        {
+            T ParseLine(StringBuilder line);
+        }
+        private class ItemLineParser : ILineParser<Item>
+        {
+            public ItemLineParser(Item.Type type)
+            {
+                Type = type;
+            }
+
+            public Item.Type Type { get; }
+
+            public Item ParseLine(StringBuilder line)
+            {
+                var startIndex = 0;
+                int id = ParseSectionAsInt(line, ref startIndex);
+                double force = ParseSectionAsDouble(line, ref startIndex);
+                double agility = ParseSectionAsDouble(line, ref startIndex);
+                double expertice = ParseSectionAsDouble(line, ref startIndex);
+                double resistance = ParseSectionAsDouble(line, ref startIndex);
+                double life = ParseSectionAsDouble(line, ref startIndex);
+                var valueHolder = new Item(id, force, agility, expertice, resistance, life, Type);
+                return valueHolder;
+            }
+
+            private static double ParseSectionAsDouble(StringBuilder line, ref int startIndex)
+            {
+                double val = 0d;
+                bool seenDot = false;
+                double fractionCounter = 10d;
+                bool flip = false;
+
+                int length = line.Length;
+                for (var index = startIndex; index < length; index++)
+                {
+                    char c = line[index];
+                    switch (c)
+                    {
+                        case '\t':
+                            startIndex = index + 1;
+                            goto exit;
+                        case '-':
+                            flip = true;
+                            continue;
+                        case '.':
+                            seenDot = true;
+                            continue;
+                        default:
+                            if (seenDot == false)
+                            {
+                                val *= 10d;
+                                val += c - '0';
+                            }
+                            else
+                            {
+                                val += (c - '0') / fractionCounter;
+                                fractionCounter *= 10d;
+                            }
+                            break;
+                    }
+                }
+            exit:
+
+                return flip ? -val : val;
+            }
+
+            private static decimal ParseSectionAsDecimal(StringBuilder line, ref int startIndex)
+            {
+                decimal val = 0m;
+                bool seenDot = false;
+                decimal fractionCounter = 10m;
+                bool flip = false;
+
+                int length = line.Length;
+                for (var index = startIndex; index < length; index++)
+                {
+                    char c = line[index];
+                    switch (c)
+                    {
+                        case '\t':
+                            startIndex = index + 1;
+                            goto exit;
+                        case '-':
+                            flip = true;
+                            continue;
+                        case '.':
+                            seenDot = true;
+                            continue;
+                        default :
+                            if (seenDot == false)
+                            {
+                                val *= 10m;
+                                val += c - '0';
+                            }
+                            else
+                            {
+                                val += (c - '0') / fractionCounter;
+                                fractionCounter *= 10m;
+                            }
+                            break;
+                    }
+                }
+                exit:
+
+                return flip ? -val : val;
+            }
+
+            private static int ParseSectionAsInt(StringBuilder line, ref int startIndex)
+            {
+                int val = 0;
+                bool flip = false;
+
+                int length = line.Length;
+                for (var index = startIndex; index < length; index++)
+                {
+                    char c = line[index];
+                    switch(c)
+                    {
+                        case '\t':
+                            startIndex = index + 1;
+                            goto exit;
+                        case '-':
+                            flip = true;
+                            continue;
+                        default:
+                            val *= 10;
+                            val += c - '0';
+                            break;
+                    }
+                }
+                exit:
+
+                return flip ? -val : val;
+            }
+        }
+    }
+}
