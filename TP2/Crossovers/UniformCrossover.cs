820ec0a47804144ddcbfb82ec7d9f79ba24cbc84
luciatorrusio <lucia.torrusio@gmail.com>
2021-03-30 20:26:48 -0300

    UniformCrossover implemented

--- TP2/Crossovers/UniformCrossover.cs
+++ TP2/Crossovers/UniformCrossover.cs
@@ -7,9 +7,22 @@ namespace TP2.Crossovers
 {
     public class UniformCrossover : ICrossover
     {
-        public ICollection<Character> Crossover(Character c1, Character c2)
+        private static Random rnd = new Random();
+         public ICollection<Character> Crossover(Character c1, Character c2)
         {
-            throw new NotImplementedException();
+            int flip;
+            int s = c1.Genes.Length;
+
+            var genes1 = new IGene[s];
+            var genes2 = new IGene[s];
+
+            for(var i = 0; i < s; i++)
+            {
+                flip = rnd.Next(2);
+                genes1[i] = flip == 0 ? c1.Genes[i] : c2.Genes[i];
+                genes2[i] = flip == 0 ? c2.Genes[i] : c1.Genes[i];
+            }
+            return new Character[] { new Character(genes1), new Character(genes2) };
         }
     }
 }
