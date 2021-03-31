3289d29b9282399a88f06a9704eec391b8e0c1c4
luciatorrusio <lucia.torrusio@gmail.com>
2021-03-30 23:35:32 -0300

    ContentFinisher Implemented

--- TP2/Finishers/ContentFinisher.cs
+++ TP2/Finishers/ContentFinisher.cs
@@ -7,9 +7,41 @@ namespace TP2.Finishers
 {
     public class ContentFinisher : IFinisher
     {
+        private int maxGenerations;
+        private int currentRepeatGenerations;
+        private int maxFitness;
+        
+        public ContentFinisher()
+        {
+            this.currentRepeatGenerations = 0;
+        }
+        public void MaxRepeatedGenerations(long generations)
+        {
+            this.maxGenerations = generations;
+            this.maxFitness = 0;
+        }
+
         public bool IsFinished(IEnumerable<Character> population, long generations, long time)
         {
-            throw new NotImplementedException();
+            if(NotChangedPopulation(population)
+                this.currentRepeatGenerations +=1;
+            return this.currentRepeatGenerations >= this.maxGenerations;
+        }
+
+        private bool NotChangedPopulation(List<Character> newPopulation)
+        {
+            int currMaxFitness = 0;
+            foreach(Character character in newPopulation)
+            {
+                if(character.Fitness > currMaxFitness)
+                    currMaxFitness = character.Fitness;
+            }
+            if(currMaxFitness != this.maxFitness)
+            {
+                this.maxFitness = currMaxFitness;
+                return false;
+            }
+            return true;
         }
     }
 }
